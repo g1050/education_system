c688227d44797f187191b1e6783fc502629f480d
sky <1005035266@qq.com>
2020-12-20 20:10:38 +0800

    老师模块合并

--- js/config.js
+++ js/config.js
@@ -1,1 +1,17 @@
 var realm_name = 'http://localhost:8080/api';
+var token = layui.data('table').token
+$.ajaxSetup({ //发送请求前触发
+    complete: function (xhr) {
+        if (xhr.responseJSON.code != 0) {
+            console.log("没有登录！")
+            layer.msg("没有登录！")
+            // location.href="login.html"
+        }else{
+            console.log("已经登录！")
+        }
+    },
+    beforeSend: function (xhr) { //可以设置自定义标头
+        console.log("ajax beforesend token:", token)
+        xhr.setRequestHeader('access_token', token);
+    }
+})
